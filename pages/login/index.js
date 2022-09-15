969c6539b6d23d26575949e9561b4fc7715846c9
Dystian En <distian.enyus@gmail.com>
2022-09-15 10:19:30 +0700

    fix: error build

--- pages/login/index.js
+++ pages/login/index.js
@@ -1,13 +1,15 @@
 import {useRouter} from "next/router";
-import {Button, Card, Form, Image, Input, message, Checkbox} from "antd";
+import {Button, Card, Form, Image, Input, message, Checkbox, Spin} from "antd";
 import Link from "next/link";
 import {useStore} from "../../components/StoreProvider";
 import {observer} from "mobx-react-lite";
+import {useState} from "react";
 
 const Login = observer(() => {
     const router = useRouter();
     const store = useStore();
     const [form] = Form.useForm();
+    const [isLoading, setIsLoading] = useState(false);
 
     const label = (text) => (
         <span className="font-bold">
@@ -18,6 +20,7 @@ const Login = observer(() => {
     const handleSubmit = async () => {
         try {
             const values = await form.validateFields();
+            setIsLoading(true)
 
             const body = {
                 username: values.username,
@@ -28,51 +31,55 @@ const Login = observer(() => {
             form.resetFields();
             message.success("Login Successfully");
             await router.push("/investment_package");
+            setIsLoading(false)
         } catch (err) {
             console.log({err});
+            setIsLoading(false)
             message.error(err.response.data.message);
         }
     };
 
     return (
-        <div className={'flex flex-col items-center h-screen max-w-lg mx-auto'}>
-            <div
-                className={'flex justify-center items-center bg-cover bg-[url("/assets/background/BG2.svg")] bg-center h-3/5 rounded-b-3xl w-full z-10'}>
-                <Image className={'w-32'} src={'/assets/logo/logo.png'} preview={false}/>
-            </div>
-            <Card className={'w-full h-4/5 -mt-10'}>
-                <div className={'flex flex-col items-center mb-10 mt-8'}>
-                    <h1 className={'text-xl font-bold'}>SELAMAT DATANG</h1>
-                    <span className={'text-base text-center opacity-50'}>Masuk Untuk Mengelola Akun Anda</span>
+        <Spin spinning={isLoading}>
+            <div className={'flex flex-col items-center h-screen max-w-lg mx-auto'}>
+                <div
+                    className={'flex justify-center items-center bg-cover bg-[url("/assets/background/BG2.svg")] bg-center h-3/5 rounded-b-3xl w-full z-10'}>
+                    <Image className={'w-32'} src={'/assets/logo/logo.png'} preview={false}/>
                 </div>
-                <Form form={form} layout={'vertical'}>
-                    <Form.Item name={'username'} label={label('Username')} rules={[{
-                        required: true,
-                        message: "Please input Username!",
-                        type: 'string',
-                    }]}>
-                        <Input placeholder={'Masukan username'}/>
-                    </Form.Item>
-                    <Form.Item name={'password'} label={label('Password')} rules={[{
-                        required: true,
-                        message: "Please input Password!",
-                    }]}>
-                        <Input.Password placeholder={'Masukan password'}/>
-                    </Form.Item>
-                    <Form.Item>
-                        {/*<Checkbox onChange={onChange}>Ingatkan saya</Checkbox>*/}
-                        <Link href="#">
-                            <a className={'float-right text-black font-semibold'}>Lupa Password?</a>
-                        </Link>
-                    </Form.Item>
-                    <Form.Item className={'text-center pt-1.5'}>
-                        <Button type={'primary'} className={'rounded-md mb-2'} size={'large'} block
-                                onClick={handleSubmit}>Masuk</Button>
-                        <span>Belum punya akun? <Link href={'/register'}>Registrasi</Link></span>
-                    </Form.Item>
-                </Form>
-            </Card>
-        </div>
+                <Card className={'w-full h-4/5 -mt-10'}>
+                    <div className={'flex flex-col items-center mb-10 mt-8'}>
+                        <h1 className={'text-xl font-bold'}>SELAMAT DATANG</h1>
+                        <span className={'text-base text-center opacity-50'}>Masuk Untuk Mengelola Akun Anda</span>
+                    </div>
+                    <Form form={form} layout={'vertical'}>
+                        <Form.Item name={'username'} label={label('Username')} rules={[{
+                            required: true,
+                            message: "Please input Username!",
+                            type: 'string',
+                        }]}>
+                            <Input placeholder={'Masukan username'}/>
+                        </Form.Item>
+                        <Form.Item name={'password'} label={label('Password')} rules={[{
+                            required: true,
+                            message: "Please input Password!",
+                        }]}>
+                            <Input.Password placeholder={'Masukan password'}/>
+                        </Form.Item>
+                        <Form.Item>
+                            {/*<Checkbox onChange={onChange}>Ingatkan saya</Checkbox>*/}
+                            <Link href="#">
+                                <a className={'float-right text-black font-semibold'}>Lupa Password?</a>
+                            </Link>
+                        </Form.Item>
+                        <Form.Item className={'text-center pt-1.5'}>
+                            <Button type={'primary'} className={'rounded-md mb-2'} size={'large'} block
+                                    onClick={handleSubmit}>Masuk</Button>
+                            <span>Belum punya akun? <Link href={'/register'}>Registrasi</Link></span>
+                        </Form.Item>
+                    </Form>
+                </Card>
+            </div>
+        </Spin>
     );
 });
 
--- pages/register/index.js
+++ pages/register/index.js
@@ -1,12 +1,14 @@
-import {Button, Card, Form, Image, Input, message} from "antd";
+import {Button, Card, Form, Image, Input, message, Spin} from "antd";
 import {authenticationRepository} from "../../repository/authentication";
 import {useRouter} from "next/router";
 import Link from "next/link";
 import {observer} from "mobx-react-lite";
+import {useState} from "react";
 
 const Register = observer(() => {
     const router = useRouter();
     const [form] = Form.useForm();
+    const [isLoading, setIsLoading] = useState(false);
 
     const label = (text) => (
         <span className="font-bold">
@@ -17,6 +19,7 @@ const Register = observer(() => {
     const handleSubmit = async () => {
         try {
             const values = await form.validateFields();
+            setIsLoading(true)
 
             const body = {
                 username: values.username,
@@ -26,43 +29,47 @@ const Register = observer(() => {
             await authenticationRepository.api.register(body);
             message.success('Register Successfully');
             await router.push('/login')
+            setIsLoading(false)
         } catch (err) {
             console.log({err})
+            setIsLoading(false)
             message.error(err.response.data.message[0]);
         }
     };
 
     return (
-        <div className={'flex flex-col items-center h-screen max-w-lg mx-auto'}>
-            <div className={'flex justify-center items-center bg-[url("/assets/background/BG.png")] bg-center h-3/5 w-full'}>
-                <Image className={'w-32'} src={'/assets/logo/logo.png'} preview={false}/>
-            </div>
-            <Card className={'w-full h-4/5 rounded-2xl -mt-10'}>
-                <div className={'flex flex-col items-center mb-10'}>
-                    <h1 className={'text-xl font-bold'}>REGISTRASI</h1>
-                    <span className={'text-base text-center opacity-50'}>Silahkan isi email aktif anda sebagai <br/> kode verifikasi</span>
+        <Spin spinning={isLoading}>
+            <div className={'flex flex-col items-center h-screen max-w-lg mx-auto'}>
+                <div className={'flex justify-center items-center bg-[url("/assets/background/BG.png")] bg-center h-3/5 w-full'}>
+                    <Image className={'w-32'} src={'/assets/logo/logo.png'} preview={false}/>
                 </div>
-                <Form form={form} layout={'vertical'}>
-                    <Form.Item name={'username'} label={label('Username')} rules={[{
-                        required: true,
-                        message: "Please input Username!",
-                    }]}>
-                        <Input placeholder={'Masukan username'}/>
-                    </Form.Item>
-                    <Form.Item name={'password'} label={label('Password')} rules={[{
-                        required: true,
-                        message: "Please input Password!",
-                    }]}>
-                        <Input.Password placeholder={'Masukan password'}/>
-                    </Form.Item>
-                    <Form.Item className={'text-center pt-5'}>
-                        <Button type={'primary'} className={'rounded-md mb-2'} size={'large'} block
-                                onClick={handleSubmit}>Buat Akun</Button>
-                        <span>Sudah punya akun? <Link href={'/login'}>Login</Link></span>
-                    </Form.Item>
-                </Form>
-            </Card>
-        </div>
+                <Card className={'w-full h-4/5 rounded-2xl -mt-10'}>
+                    <div className={'flex flex-col items-center mb-10'}>
+                        <h1 className={'text-xl font-bold'}>REGISTRASI</h1>
+                        <span className={'text-base text-center opacity-50'}>Silahkan isi email aktif anda sebagai <br/> kode verifikasi</span>
+                    </div>
+                    <Form form={form} layout={'vertical'}>
+                        <Form.Item name={'username'} label={label('Username')} rules={[{
+                            required: true,
+                            message: "Please input Username!",
+                        }]}>
+                            <Input placeholder={'Masukan username'}/>
+                        </Form.Item>
+                        <Form.Item name={'password'} label={label('Password')} rules={[{
+                            required: true,
+                            message: "Please input Password!",
+                        }]}>
+                            <Input.Password placeholder={'Masukan password'}/>
+                        </Form.Item>
+                        <Form.Item className={'text-center pt-5'}>
+                            <Button type={'primary'} className={'rounded-md mb-2'} size={'large'} block
+                                    onClick={handleSubmit}>Buat Akun</Button>
+                            <span>Sudah punya akun? <Link href={'/login'}>Login</Link></span>
+                        </Form.Item>
+                    </Form>
+                </Card>
+            </div>
+        </Spin>
     )
 });
 
