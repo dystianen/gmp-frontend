836ffae07bb1bd84d49b74cdaf4feebda29b780d
Anang20 <anangsyah766@gmail.com>
2022-09-19 19:24:19 +0700

    chore: add users repository

---
+++ repository/users.js
@@ -0,0 +1,47 @@
+import { http } from "../utils/http";
+import useSWR from "swr";
+
+const url = {
+    getAllUser: () => "/users/",
+    getUserId: (id) => `/users/detail/${id}`,
+    editUser: (id) => `/users/${id}`,
+    deleteUser: (id) => `/users/${id}`,
+    uploadUserImage: () => `/file/upload`,
+    changePassword: () => `/users/change-password`,
+    binaryTree: () => `/users/binary-tree`,
+    sunTree: () => `/users/sun-tree`,
+    getBalanceUSDT: () => `/users/balance`,
+    getBalanceGMP: () => `/users/balance-gmp`,
+};
+
+const hooks = {
+    useGetAllUser() {
+        return useSWR(url.getAllUser(), http.fetcher);
+    },
+    useGetUserDetail(id) {
+        return useSWR(url.getUserId(id), http.fetcher);
+    },
+    userGetBalanceUSDT() {
+        return useSWR(url.getBalanceUSDT(), http.fetcher);
+    },
+    userGetBalanceGMP() {
+        return useSWR(url.getBalanceGMP(), http.fetcher);
+    },
+}
+
+const api = {
+    updateProfile(id, data) {
+        return http.put(url.editUser(id), data);
+    },    
+    uploadUserImage(file) {
+        const formData = new FormData();
+        formData.append("file", file);
+        return http.upload(url.uploadUserImage()).send(formData);
+    },
+};
+
+export const userRepository = {
+    url,
+    hooks,
+    api,
+};
