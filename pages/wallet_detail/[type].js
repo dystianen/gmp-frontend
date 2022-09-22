477813528dde1792b5e0c17c32bd681718fd3b04
Anang20 <anangsyah766@gmail.com>
2022-09-22 15:21:05 +0700

    fix: redesign wallet

--- pages/wallet_detail/[type].js
+++ pages/wallet_detail/[type].js
@@ -60,140 +60,138 @@ const WalletDetails = observer(() => {
 
     return (
         <>
-            {/* <div className="relative bg-[#FAFAFA] min-h-screen max-w-lg bg-center w-full mx-auto px-6 rounded-t"> */}
-                <div className={'relative flex flex-col justify-center items-center bg-primary bg-center h-2/6 lg:h-[40%] w-full rounded-b-[30px]'}>
-                    <div className={'grid grid-cols-3 w-5/6 z-10 -mt-28'}>
-                        <Button className={'flex justify-center items-center rounded-lg p-0 h-10 w-12'}
-                                onClick={() => router.back()}>
-                            <BiArrowBack  className={'text-lg'}/>
-                        </Button>
-                        <div className="flex items-center col-span-2">
-                            {type === "GMP" ? (
-                                <Image src={'/assets/logo/mini-logo2.png'} className={'px-2'}  preview={false}/>
-                            ) : (        
-                                <Image src={'/assets/logo/theter2.png'} className={'px-2'}  preview={false}/>
-                            )}
-                        <span className={'w-full text-2xl font-bold text-white'}>{type}</span>
-                        </div>
-                    </div>
-                    <div className="absolute h-full">
-                        <Image src={'/assets/background/Particle1.png'} preview={false}/>
-                    </div>
-                    <div className="absolute top-0 left-0">
-                        <Image src={'/assets/background/BGYellowTop.svg'} preview={false}/>
-                    </div>
-                    <div className="absolute bottom-0 right-0">
-                        <Image className={'-mb-[6px] w-36 h-36 rounded-b-[30px]'}
-                            src={'/assets/background/BGYellowBot2.png'} preview={false}/>
+            <div className={'relative flex flex-col justify-center items-center bg-primary bg-center h-2/6 lg:h-[40%] w-full rounded-b-[30px]'}>
+                <div className={'grid grid-cols-3 w-5/6 z-10 -mt-28'}>
+                    <Button className={'flex justify-center items-center rounded-lg p-0 h-10 w-12'}
+                            onClick={() => router.back()}>
+                        <BiArrowBack  className={'text-lg'}/>
+                    </Button>
+                    <div className="flex items-center col-span-2">
+                        {type === "GMP" ? (
+                            <Image src={'/assets/logo/mini-logo2.png'} className={'px-2'}  preview={false}/>
+                        ) : (        
+                            <Image src={'/assets/logo/theter2.png'} className={'px-2'}  preview={false}/>
+                        )}
+                    <span className={'w-full text-2xl font-bold text-white'}>{type}</span>
                     </div>
                 </div>
-                
-                {type === "GMP" ? (
-                    <Card className={"-mt-[110px] lg:-mt-[132px] h-[170px] lg:h-48 rounded-xl border-none mx-10 w-4/5 bg-[url('/assets/background/GMT.png')] bg-transparent bg-top bg-cover"}>
-                        <div className={'flex items-center gap-2 -mt-3'}>
-                            <Image src={'/assets/logo/mini-logo.png'} preview={false}/>
-                            <span className={'text-white font-semibold text-sm leading-8'}>{type}</span>
+                <div className="absolute h-full">
+                    <Image src={'/assets/background/Particle1.png'} preview={false}/>
+                </div>
+                <div className="absolute top-0 left-0">
+                    <Image src={'/assets/background/BGYellowTop.svg'} preview={false}/>
+                </div>
+                <div className="absolute bottom-0 right-0">
+                    <Image className={'-mb-[6px] w-36 h-36 rounded-b-[30px]'}
+                        src={'/assets/background/BGYellowBot2.png'} preview={false}/>
+                </div>
+            </div>
+            
+            {type === "GMP" ? (
+                <Card className={"-mt-[110px] lg:-mt-[132px] h-[170px] lg:h-48 rounded-xl border-none mx-10 w-4/5 bg-[url('/assets/background/GMT.png')] bg-transparent bg-top bg-cover"}>
+                    <div className={'flex items-center gap-2 -mt-3'}>
+                        <Image src={'/assets/logo/mini-logo.png'} preview={false}/>
+                        <span className={'text-white font-semibold text-sm leading-8'}>{type}</span>
+                    </div>
+                    <div className={'absolute'}>
+                        <div className={'font-semibold text-sm leading-4 text-white mt-2.5'}>
+                            Balance
                         </div>
-                        <div className={'absolute'}>
-                            <div className={'font-semibold text-sm leading-4 text-white mt-2.5'}>
-                                Balance
-                            </div>
-                            <h2 className={'font-semibold text-3xl text-white z-10'}>
-                                <FormatNumber value={dataBalanceGMP?.data}/>
-                            </h2>
+                        <h2 className={'font-semibold text-3xl text-white z-10'}>
+                            <FormatNumber value={dataBalanceGMP?.data}/>
+                        </h2>
+                    </div>
+                    <div className="absolute px-6 w-full right-0 left-0 rounded-b-xl bottom-0" style={{ backgroundColor: 'rgba(254, 155, 11, 0.4)' }}>
+                        <p className={'text-sm font-semibold text-white mb-[1px] pt-2'}>Transaksi Terakhir</p>
+                        {lastTransactionsGMP?.data === null ? (
+                            <p className="pl-14 text-white">-</p>
+                        ) : (
+                            <p className={'text-white font-semibold text-base'}>{moment(lastTransactionsGMP?.data?.updateAt).format('DD MMMM YYYY')}</p>
+                        )}
+                    </div>
+                </Card>
+            ) : (
+                <Card className={"-mt-[101px] lg:-mt-[123px] h-[170px] lg:h-48 bg-transparent rounded-xl border-none mx-10 w-4/5 bg-[url('/assets/background/USDT.png')] bg-cover bg-top opacity-90"}>
+                    <div className={'flex items-center gap-2 -mt-3'}>
+                        <Image src={'/assets/logo/theter.png'} preview={false}/>
+                        <span className={'text-white font-semibold text-sm leading-8'}>{type}</span>
+                    </div>
+                    <div className={'absolute'}>
+                        <div className={'font-semibold text-sm leading-4 text-white mt-2.5'}>
+                            Balance
                         </div>
-                        <div className="absolute px-6 w-full right-0 left-0 rounded-b-xl bottom-0" style={{ backgroundColor: 'rgba(254, 155, 11, 0.4)' }}>
-                            <p className={'text-sm font-semibold text-white mb-[1px] pt-2'}>Transaksi Terakhir</p>
-                            {lastTransactionsGMP?.data === null ? (
-                                <p className="pl-14 text-white">-</p>
-                            ) : (
-                                <p className={'text-white font-semibold text-base'}>{moment(lastTransactionsGMP?.data?.updateAt).format('DD MMMM YYYY')}</p>
-                            )}
-                        </div>
-                    </Card>
-                ) : (
-                    <Card className={"-mt-[101px] lg:-mt-[123px] h-[170px] lg:h-48 bg-transparent rounded-xl border-none mx-10 w-4/5 bg-[url('/assets/background/USDT.png')] bg-cover bg-top opacity-90"}>
-                        <div className={'flex items-center gap-2 -mt-3'}>
-                            <Image src={'/assets/logo/theter.png'} preview={false}/>
-                            <span className={'text-white font-semibold text-sm leading-8'}>{type}</span>
-                        </div>
-                        <div className={'absolute'}>
-                            <div className={'font-semibold text-sm leading-4 text-white mt-2.5'}>
-                                Balance
-                            </div>
-                            <h2 className={'font-semibold text-3xl text-white z-10'}>
-                                <FormatNumber value={dataBalanceUSDT?.data}/>
-                            </h2>
-                        </div>
-                        <div className="absolute px-6 w-full right-0 left-0 rounded-b-xl bottom-0" style={{ backgroundColor: 'rgba(0, 0, 0, 0.4)'}}>
-                            <p className={'text-sm font-semibold text-white mb-[1px] pt-2'}>Transaksi Terakhir</p>
-                            {lastTransactionsUSDT?.data === null ? (
-                                <p className="pl-14 text-white">-</p>
-                            ) : (
-                                <p className={'text-white font-semibold text-base'}>{moment(lastTransactionsUSDT?.data?.updateAt).format('DD MMMM YYYY')}</p>
-                            )}
-                        </div>
-                    </Card>
-                )}
+                        <h2 className={'font-semibold text-3xl text-white z-10'}>
+                            <FormatNumber value={dataBalanceUSDT?.data}/>
+                        </h2>
+                    </div>
+                    <div className="absolute px-6 w-full right-0 left-0 rounded-b-xl bottom-0" style={{ backgroundColor: 'rgba(0, 0, 0, 0.4)'}}>
+                        <p className={'text-sm font-semibold text-white mb-[1px] pt-2'}>Transaksi Terakhir</p>
+                        {lastTransactionsUSDT?.data === null ? (
+                            <p className="pl-14 text-white">-</p>
+                        ) : (
+                            <p className={'text-white font-semibold text-base'}>{moment(lastTransactionsUSDT?.data?.updateAt).format('DD MMMM YYYY')}</p>
+                        )}
+                    </div>
+                </Card>
+            )}
 
-                {dataTransaction?.data.length === 0 ? (
-                    <div className={'mt-8 pb-28 px-10'}>
-                        <p className={'font-semibold text-lg'}>Riwayat Transaksi</p>
-                        <div className="relative">
-                            <Form>
-                                <Form.Item>
-                                    <DatePicker.RangePicker 
-                                        className="mr-2 rounded-md" 
-                                        picker="month" 
-                                        onChange={(e) => filterDate(e?.[0], e?.[1])}
-                                    />
-                                </Form.Item>
-                            </Form>
-                        </div>
-                        <div className={'flex justify-center'}>
-                            Anda Belum Memiliki Riwayat Transaksi
-                        </div>
+            {dataTransaction?.data.length === 0 ? (
+                <div className={'mt-8 pb-28 px-10'}>
+                    <p className={'font-semibold text-lg'}>Riwayat Transaksi</p>
+                    <div className="relative">
+                        <Form>
+                            <Form.Item>
+                                <DatePicker.RangePicker 
+                                    className="mr-2 rounded-md" 
+                                    picker="month" 
+                                    onChange={(e) => filterDate(e?.[0], e?.[1])}
+                                />
+                            </Form.Item>
+                        </Form>
                     </div>
-                ) : (
-                    <div className={'mt-8 pb-28 px-10'}>
-                        <p className={'font-semibold text-lg'}>Riwayat Transaksi</p>
-                        <div className="relative">
-                            <Form>
-                                <Form.Item>
-                                    <DatePicker.RangePicker 
-                                        className="mr-2 rounded-md" 
-                                        picker="month" 
-                                        onChange={(e) => filterDate(e?.[0], e?.[1])}
-                                    />
-                                </Form.Item>
-                            </Form>
-                        </div>
-                        {dataTransaction?.data.map((value, index) => (
-                            <div key={index} onClick={() => router.push(`/wallet_detail/transaction_detail/${value.id}`)} className={'grid grid-rows-2 grid-flow-col mb-2 mt-4 cursor-pointer'}>
-                                {value?.type === 0 ? (
-                                    <div className={'font-semibold text-base mb-1'}>Buy Package</div>
-                                ) : value?.type === 1 ? (
-                                    <div className={'font-semibold text-base mb-1'}>Distribute Pair</div>
-                                ) : value?.type === 2 ? (       
-                                    <div className={'font-semibold text-base mb-1'}>Stake Result</div>
-                                ) : value?.type === 3 ? (        
-                                    <div className={'font-semibold text-base mb-1'}>Stake Level Result</div>
-                                ) : value?.type === 4 ? (
-                                    <div className={'font-semibold text-base mb-1'}>Move Internal GMP</div>
-                                ) : value?.type === 5 ? (
-                                    <div className={'font-semibold text-base mb-1'}>Stake External GMP</div>
-                                ) : (
-                                    <div className={'font-semibold text-base mb-1'}>Move External USDT</div>
-                                )}
-                                <div className={'text-sm font-normal text-slate-600'}>{formatDate(new Date(value.createdAt))}</div>
-                                <div className={'row-span-3 col-span-2 text-lg font-semibold pb-8'}>
-                                    <p className={'text-right text-green-500 mb-1'}>{value.amount} {value?.currency}</p>
-                                </div>
+                    <div className={'flex justify-center'}>
+                        Anda Belum Memiliki Riwayat Transaksi
+                    </div>
+                </div>
+            ) : (
+                <div className={'mt-8 pb-28 px-10'}>
+                    <p className={'font-semibold text-lg'}>Riwayat Transaksi</p>
+                    <div className="relative">
+                        <Form>
+                            <Form.Item>
+                                <DatePicker.RangePicker 
+                                    className="mr-2 rounded-md" 
+                                    picker="month" 
+                                    onChange={(e) => filterDate(e?.[0], e?.[1])}
+                                />
+                            </Form.Item>
+                        </Form>
+                    </div>
+                    {dataTransaction?.data.map((value, index) => (
+                        <div key={index} onClick={() => router.push(`/wallet_detail/transaction_detail/${value.id}`)} className={'grid grid-rows-2 grid-flow-col mb-2 mt-4 cursor-pointer'}>
+                            {value?.type === 0 ? (
+                                <div className={'font-semibold text-base mb-1'}>Buy Package</div>
+                            ) : value?.type === 1 ? (
+                                <div className={'font-semibold text-base mb-1'}>Distribute Pair</div>
+                            ) : value?.type === 2 ? (       
+                                <div className={'font-semibold text-base mb-1'}>Stake Result</div>
+                            ) : value?.type === 3 ? (        
+                                <div className={'font-semibold text-base mb-1'}>Stake Level Result</div>
+                            ) : value?.type === 4 ? (
+                                <div className={'font-semibold text-base mb-1'}>Move Internal GMP</div>
+                            ) : value?.type === 5 ? (
+                                <div className={'font-semibold text-base mb-1'}>Stake External GMP</div>
+                            ) : (
+                                <div className={'font-semibold text-base mb-1'}>Move External USDT</div>
+                            )}
+                            <div className={'text-sm font-normal text-slate-600'}>{moment(value?.createdAt).format('DD MMMM YYYY')}</div>
+                            <div className={'row-span-3 col-span-2 text-lg font-semibold pb-8'}>
+                                <p className={'text-right text-green-500 mb-1'}>{value.amount} {value?.currency}</p>
                             </div>
-                        ))}
-                    </div>
-                )}
-            {/* </div> */}
+                        </div>
+                    ))}
+                </div>
+            )}
         </>
     )
 })
